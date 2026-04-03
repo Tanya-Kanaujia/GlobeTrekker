a3544cc3d83dcb8230705745b3dad72276a96531
Tanya Kanaujia <tanyakanaujia851@gmail.com>
2026-04-03 21:23:28 +0530

    Fixing navbar.ejs

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
 	res.locals.success = req.flash("success");
 	res.locals.error = req.flash("error");
-	res.locals.currentUser = req.user || null;
+	res.locals.currentUser = req.user;
 	next();
 });
 
